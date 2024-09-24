c3f71de3ef9e1a84e6a8001e9a9dceaf8e849fce
Matthew Larson <matthewjlar@gmail.com>
2024-09-24 09:54:44 -0500

    Fix error stack cleanup in non-main threads

--- src/H5Eint.c
+++ src/H5Eint.c
@@ -331,13 +331,14 @@ H5E_term_package(void)
 H5E_stack_t *
 H5E__get_stack(void)
 {
+    H5TS_tl_value_t *tl_value = NULL;
     H5E_stack_t *estack = NULL;
 
     FUNC_ENTER_PACKAGE_NOERR
 
-    estack = (H5E_stack_t *)H5TS_get_thread_local_value(H5TS_errstk_key_g);
+    tl_value = (H5TS_tl_value_t *)H5TS_get_thread_local_value(H5TS_errstk_key_g);
 
-    if (!estack) {
+    if (!tl_value) {
         /* No associated value with current thread - create one */
 #ifdef H5_HAVE_WIN_THREADS
         /* Win32 has to use LocalAlloc to match the LocalFree in DllMain */
@@ -353,12 +354,23 @@ H5E__get_stack(void)
         /* Set the thread-specific info */
         H5E__set_default_auto(estack);
 
+        /* Set up threadlocal wrapper */
+        tl_value = (H5TS_tl_value_t *)malloc(sizeof(H5TS_tl_value_t));
+        assert(tl_value);
+
+        tl_value->type  = H5TS_ERRSTK;
+        tl_value->value = (void *)estack;
+
         /* (It's not necessary to release this in this API, it is
          *      released by the "key destructor" set up in the H5TS
          *      routines.  See calls to pthread_key_create() in H5TS.c -QAK)
          */
-        H5TS_set_thread_local_value(H5TS_errstk_key_g, (void *)estack);
-    } /* end if */
+        H5TS_set_thread_local_value(H5TS_errstk_key_g, (void *) tl_value);
+    }
+    else {
+        estack = (H5E_stack_t *)tl_value->value;
+        assert(estack);
+    }
 
     /* Set return value */
     FUNC_LEAVE_NOAPI(estack)
@@ -2032,10 +2044,9 @@ H5E__clear_stack(H5E_stack_t *estack)
             HGOTO_ERROR(H5E_ERROR, H5E_CANTGET, FAIL, "can't get current error stack");
 
     } else {
-
         bool have_global_mutex;
         
-        /* Verify that the have the global lock.  */
+        /* Verify that we have the global lock. */
         if ( H5TS_have_mutex(&H5_g.init_lock, &have_global_mutex) < 0 )
 
             HGOTO_ERROR(H5E_LIB, H5E_CANTGET, FAIL, "Can't determine whether we have the global mutex");
