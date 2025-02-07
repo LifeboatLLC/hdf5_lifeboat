Krande/hdf5

Add new -testexpress option to testframe testing framework (#5286)
Adds new -testexpress command-line option to the testframe testing
framework to allow setting or overriding of the TestExpress level
at runtime

Adds macros for the different currently defined TestExpress levels

test/btree2.c
@@ -9928,10 +9928,10 @@ main(void)
     localTestExpress = h5_get_testexpress();
 
     /* For the Direct I/O driver, skip intensive tests due to poor performance */
-    if (localTestExpress < 2 && !strcmp(driver_name, "direct"))
-        localTestExpress = 2;
+    if (localTestExpress < H5_TEST_EXPRESS_QUICK && !strcmp(driver_name, "direct"))
+        localTestExpress = H5_TEST_EXPRESS_QUICK;
 
-    if (localTestExpress > 0)
+    if (localTestExpress > H5_TEST_EXPRESS_EXHAUSTIVE)
         printf("***Express test mode %d.  Some tests may be skipped\n", localTestExpress);
 
     /* Initialize v2 B-tree creation parameters */
@@ -9968,7 +9968,7 @@ main(void)
         nerrors += test_insert_level2_2internal_split(fapl, &cparam, &tparam);
         nerrors += test_insert_level2_3internal_redistrib(fapl, &cparam, &tparam);
         nerrors += test_insert_level2_3internal_split(fapl, &cparam, &tparam);
-        if (localTestExpress > 1)
+        if (localTestExpress > H5_TEST_EXPRESS_FULL)
             printf("***Express test mode on.  test_insert_lots skipped\n");
         else
             nerrors += test_insert_lots(fapl, &cparam, &tparam);
@@ -9982,7 +9982,7 @@ main(void)
         nerrors += test_update_level1_3leaf_redistrib(fapl, &cparam2, &tparam);
         nerrors += test_update_level1_middle_split(fapl, &cparam2, &tparam);
         nerrors += test_update_make_level2(fapl, &cparam2, &tparam);
-        if (localTestExpress > 1)
+        if (localTestExpress > H5_TEST_EXPRESS_FULL)
             printf("***Express test mode on.  test_update_lots skipped\n");
         else
             nerrors += test_update_lots(fapl, &cparam2, &tparam);
@@ -10009,7 +10009,7 @@ main(void)
         nerrors += test_remove_level2_2internal_merge_right(fapl, &cparam, &tparam);
         nerrors += test_remove_level2_3internal_merge(fapl, &cparam, &tparam);
         nerrors += test_remove_level2_collapse_right(fapl, &cparam, &tparam);
-        if (localTestExpress > 1)
+        if (localTestExpress > H5_TEST_EXPRESS_FULL)
             printf("***Express test mode on.  test_remove_lots skipped\n");
         else
             nerrors += test_remove_lots(driver_name, fapl, &cparam);

test/testframe.c
@@ -208,18 +208,21 @@ TestUsage(FILE *stream)
     fprintf(stream, "              [-o[nly] name]+ \n");
     fprintf(stream, "              [-b[egin] name] \n");
     fprintf(stream, "              [-[max]t[hreads]]  \n");
+    fprintf(stream, "              [-[testex]p[ress] (0-3)]  \n");
     fprintf(stream, "              [-s[ummary]]  \n");
     fprintf(stream, "              [-c[leanoff]]  \n");
     fprintf(stream, "              [-h[elp]]  \n");
     fprintf(stream, "\n\n");
-    fprintf(stream, "verbose     controls the amount of information displayed\n");
-    fprintf(stream, "exclude     to exclude tests by name\n");
-    fprintf(stream, "only        to name tests which should be run\n");
-    fprintf(stream, "begin       start at the name of the test given\n");
-    fprintf(stream, "maxthreads  maximum number of threads to be used by multi-thread tests\n");
-    fprintf(stream, "summary     prints a summary of test results at the end\n");
-    fprintf(stream, "cleanoff    does not delete *.hdf files after execution of tests\n");
-    fprintf(stream, "help        print out this information\n");
+    fprintf(stream, "verbose      controls the amount of information displayed\n");
+    fprintf(stream, "exclude      to exclude tests by name\n");
+    fprintf(stream, "only         to name tests which should be run\n");
+    fprintf(stream, "begin        start at the name of the test given\n");
+    fprintf(stream, "maxthreads   maximum number of threads to be used by multi-thread tests\n");
+    fprintf(stream, "testexpress  set the TestExpress level for expediting tests\n");
+    fprintf(stream, "               lower values run more tests; higher values skip more tests\n");
+    fprintf(stream, "summary      prints a summary of test results at the end\n");
+    fprintf(stream, "cleanoff     does not delete *.hdf files after execution of tests\n");
+    fprintf(stream, "help         print out this information\n");
     if (TestPrivateUsage_g) {
         fprintf(stream, "\nExtra options\n");
         TestPrivateUsage_g(stream);
@@ -383,6 +386,42 @@ TestParseCmdLine(int argc, char *argv[])
                 ret_value = FAIL;
                 goto done;
             }
+        }
+        else if ((strcmp(*argv, "-testexpress") == 0) || (strcmp(*argv, "-t") == 0)) {
+            long test_express_level;
+
+            if (argc <= 0 || !argv[1]) {
+                if (TestFrameworkProcessID_g == 0)
+                    fprintf(stderr, "no argument given to -testexpress option\n");
+                ret_value = FAIL;
+                goto done;
+            }
+
+            --argc;
+            ++argv;
+
+            errno              = 0;
+            test_express_level = strtol(*argv, NULL, 10);
+            if (errno != 0) {
+                if (TestFrameworkProcessID_g == 0)
+                    fprintf(stderr, "error while parsing value (%s) specified for TestExpress level\n",
+                            *argv);
+                ret_value = FAIL;
+                goto done;
+            }
+            if (test_express_level < 0) {
+                if (TestFrameworkProcessID_g == 0)
+                    fprintf(stderr, "invalid value (%ld) specified for TestExpress level\n",
+                            test_express_level);
+                ret_value = FAIL;
+                goto done;
+            }
+
+            /* Clamp value to current highest TestExpress level */
+            if (test_express_level > H5_TEST_EXPRESS_SMOKE_TEST)
+                test_express_level = H5_TEST_EXPRESS_SMOKE_TEST;
+
+            SetTestExpress((int)test_express_level);
         }
         else {
             /* non-standard option.  Break out. */

LifeboatLLC/hdf5_lifeboat

test/btree2.c
@@ -9928,11 +9928,11 @@ main(void)
     ExpressMode = h5_get_testexpress();
 
     /* For the Direct I/O driver, skip intensive tests due to poor performance */
-    if (!HDstrcmp(envval, "direct"))
-        ExpressMode = 2;
+    if (ExpressMode < H5_TEST_EXPRESS_QUICK && !strcmp(envval, "direct"))
+        ExpressMode = H5_TEST_EXPRESS_QUICK;
 
-    if (ExpressMode > 1)
-        printf("***Express test mode on.  Some tests may be skipped\n");
+    if (ExpressMode > H5_TEST_EXPRESS_EXHAUSTIVE)
+        printf("***Express test mode %d.  Some tests may be skipped\n", ExpressMode);
 
     /* Initialize v2 B-tree creation parameters */
     init_cparam(&cparam, &cparam2);
@@ -9968,7 +9968,7 @@ main(void)
         nerrors += test_insert_level2_2internal_split(fapl, &cparam, &tparam);
         nerrors += test_insert_level2_3internal_redistrib(fapl, &cparam, &tparam);
         nerrors += test_insert_level2_3internal_split(fapl, &cparam, &tparam);
-        if (ExpressMode > 1)
+        if (ExpressMode > H5_TEST_EXPRESS_FULL)
             printf("***Express test mode on.  test_insert_lots skipped\n");
         else
             nerrors += test_insert_lots(fapl, &cparam, &tparam);
@@ -9982,7 +9982,7 @@ main(void)
         nerrors += test_update_level1_3leaf_redistrib(fapl, &cparam2, &tparam);
         nerrors += test_update_level1_middle_split(fapl, &cparam2, &tparam);
         nerrors += test_update_make_level2(fapl, &cparam2, &tparam);
-        if (ExpressMode > 1)
+        if (ExpressMode > H5_TEST_EXPRESS_FULL)
             printf("***Express test mode on.  test_update_lots skipped\n");
         else
             nerrors += test_update_lots(fapl, &cparam2, &tparam);
@@ -10009,7 +10009,7 @@ main(void)
         nerrors += test_remove_level2_2internal_merge_right(fapl, &cparam, &tparam);
         nerrors += test_remove_level2_3internal_merge(fapl, &cparam, &tparam);
         nerrors += test_remove_level2_collapse_right(fapl, &cparam, &tparam);
-        if (ExpressMode > 1)
+        if (ExpressMode > H5_TEST_EXPRESS_FULL)
             printf("***Express test mode on.  test_remove_lots skipped\n");
         else
             nerrors += test_remove_lots(envval, fapl, &cparam);

test/testframe.c
@@ -238,18 +238,21 @@ TestUsage(FILE *stream)
     fprintf(stream, "              [-o[nly] name]+ \n");
     fprintf(stream, "              [-b[egin] name] \n");
     fprintf(stream, "              [-[max]t[hreads]]  \n");
+    fprintf(stream, "              [-[testex]p[ress] (0-3)]  \n");
     fprintf(stream, "              [-s[ummary]]  \n");
     fprintf(stream, "              [-c[leanoff]]  \n");
     fprintf(stream, "              [-h[elp]]  \n");
     fprintf(stream, "\n\n");
-    fprintf(stream, "verbose     controls the amount of information displayed\n");
-    fprintf(stream, "exclude     to exclude tests by name\n");
-    fprintf(stream, "only        to name tests which should be run\n");
-    fprintf(stream, "begin       start at the name of the test given\n");
-    fprintf(stream, "maxthreads  maximum number of threads to be used by multi-thread tests\n");
-    fprintf(stream, "summary     prints a summary of test results at the end\n");
-    fprintf(stream, "cleanoff    does not delete *.hdf files after execution of tests\n");
-    fprintf(stream, "help        print out this information\n");
+    fprintf(stream, "verbose      controls the amount of information displayed\n");
+    fprintf(stream, "exclude      to exclude tests by name\n");
+    fprintf(stream, "only         to name tests which should be run\n");
+    fprintf(stream, "begin        start at the name of the test given\n");
+    fprintf(stream, "maxthreads   maximum number of threads to be used by multi-thread tests\n");
+    fprintf(stream, "testexpress  set the TestExpress level for expediting tests\n");
+    fprintf(stream, "               lower values run more tests; higher values skip more tests\n");
+    fprintf(stream, "summary      prints a summary of test results at the end\n");
+    fprintf(stream, "cleanoff     does not delete *.hdf files after execution of tests\n");
+    fprintf(stream, "help         print out this information\n");
     if (TestPrivateUsage_g) {
         fprintf(stream, "\nExtra options\n");
         TestPrivateUsage_g(stream);
@@ -422,6 +425,42 @@ TestParseCmdLine(int argc, char *argv[])
                 ret_value = FAIL;
                 goto done;
             }
+        }
+        else if ((strcmp(*argv, "-testexpress") == 0) || (strcmp(*argv, "-p") == 0)) {
+            long test_express_level;
+
+            if (argc <= 0 || !argv[1]) {
+                if (TestFrameworkProcessID_g == 0)
+                    fprintf(stderr, "no argument given to -testexpress option\n");
+                ret_value = FAIL;
+                goto done;
+            }
+
+            --argc;
+            ++argv;
+
+            errno              = 0;
+            test_express_level = strtol(*argv, NULL, 10);
+            if (errno != 0) {
+                if (TestFrameworkProcessID_g == 0)
+                    fprintf(stderr, "error while parsing value (%s) specified for TestExpress level\n",
+                            *argv);
+                ret_value = FAIL;
+                goto done;
+            }
+            if (test_express_level < 0) {
+                if (TestFrameworkProcessID_g == 0)
+                    fprintf(stderr, "invalid value (%ld) specified for TestExpress level\n",
+                            test_express_level);
+                ret_value = FAIL;
+                goto done;
+            }
+
+            /* Clamp value to current highest TestExpress level */
+            if (test_express_level > H5_TEST_EXPRESS_SMOKE_TEST)
+                test_express_level = H5_TEST_EXPRESS_SMOKE_TEST;
+
+            SetTestExpress((int)test_express_level);
         }
         else {
             /* non-standard option.  Break out. */